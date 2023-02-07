53166855f847ebe346add9949feea92b93ff4a8b
mirisurprize <miriamaynalem@hotmail.com>
2023-02-07 16:26:17 +0100

    module

---
+++ module.js
@@ -0,0 +1,15 @@
+//let chosenL = "en";
+const translate ={
+    en:{
+        mainTitle:"You picked english!"
+    },
+    no:{
+        mainTitle:"Du har valgt norsk!"
+    },
+    ita:{
+        mainTitle:"Hai scelto italiano!"
+    }
+
+}
+
+export default translate;
